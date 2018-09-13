d8480be4997a0947615c3a10fa06cc00fe908d2f
realfakeuser <tim.niewerth@tobit.software>
2018-09-13 15:00:53 +0200

    basic structure initialized

---
+++ RestApiModel/Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestApiModel.Stores;
+
+namespace RestApiModel.Controllers
+{
+    [Route("api/company")]
+    public class CompanyController : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetCompany()
+        {
+            return new JsonResult(tempFillModelsWithDummyData.Current.Company);
+        }
+        [HttpGet("{id}")]
+        public JsonResult GetCompany(int id)
+        {
+            return new JsonResult(
+                CompanyDataStore.Current.Company.FirstOrDefault(c => c.Id == id)
+                );
+        }
+    }
+}
--- RestApiModel/Startup.cs
+++ RestApiModel/Startup.cs
@@ -15,6 +15,7 @@ namespace RestApiModel
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +25,11 @@ namespace RestApiModel
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                throw new Exception("Example exception");
-            });
+            app.UseMvc();
+            //app.Run(async (context) =>
+            //{
+                //throw new Exception("Example exception");
+            //});
             /*
             app.Run(async (context) =>
             {
