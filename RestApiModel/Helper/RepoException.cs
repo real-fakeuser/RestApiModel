8a0fa71769ba2ca54684df590b91be830ffb6977
realfakeuser <tim.niewerth@tobit.software>
2018-09-14 13:56:58 +0200

    ka was passiert ist

---
+++ RestApiModel/Helper/RepoException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestApiModel.Model;
+
+namespace RestApiModel.Helper
+{
+    [Serializable]
+    /* public class RepoException : Exception
+     {
+         EnumResultTypes EnumResultTypes = new EnumResultTypes();
+         public EnumResultTypes Type { get; set; }
+         public RepoException(EnumResultTypes type)
+         {
+             Type = type;
+         }
+         public RepoException(string message, EnumResultTypes type) : base(message)
+         {
+             Type = type;
+         }
+         public RepoException(string message, Exception inner) : base(message, inner) { }
+         protected RepoException(
+             System.Runtime.Serialization.SerializationInfo info,
+             System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+     }*/
+    public class RepoException : Exception
+    {
+        EnumResultTypes EnumResultTypes = new EnumResultTypes();
+        public EnumResultTypes Type { get; set; }
+        public RepoException(EnumResultTypes type)
+        {
+            Type = type;
+        }
+        public RepoException(string message, EnumResultTypes type) : base(message)
+        {
+            Type = type;
+        }
+        public RepoException(string message, Exception inner) : base(message, inner) { }
+        protected RepoException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+
+
+    public enum UpdateResultType { OK = 1, SQLERROR, NOTFOUND, INVALIDEARGUMENT, ERROR }
+
+}
---
+++ RestApiModel/Model/EnumResultTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiModel.Model
+{
+    public enum EnumResultTypes
+    {
+        OK = 1,
+        SQLERROR,
+        NOTFOUND,
+        INVALIDARGUMENT,
+        ERROR
+    }
+}
