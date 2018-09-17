99b58d7c7482aec79dc183f62a28da7f117d44fb
realfakeuser <tim.niewerth@tobit.software>
2018-09-17 14:35:17 +0200

    code cleaned up

--- RestApiModel/Controller/CompanyController.cs
+++ RestApiModel/Controller/CompanyController.cs
@@ -22,7 +22,7 @@ namespace RestApiModel.Controllers
             _companyRepo = companyRepo;
         }
 
-        //CompanyRepo repo = new CompanyRepo();
+
         [HttpGet()]                                                             //Read
         public IActionResult GetCompany()
         {
@@ -54,8 +54,8 @@ namespace RestApiModel.Controllers
                         return StatusCode(StatusCodes.Status501NotImplemented);
                 }
             }
-
         }
+
 
         [HttpGet("{id}")]                                                       //Read
         public IActionResult GetCompany(int Id)
@@ -78,8 +78,9 @@ namespace RestApiModel.Controllers
             {
                 return StatusCode(StatusCodes.Status400BadRequest);
             }
-
         }
+
+
         [HttpPost()]                                                            //Create
         public IActionResult Add([FromBody] Company value)
         {
@@ -101,11 +102,10 @@ namespace RestApiModel.Controllers
             {
                 return StatusCode(StatusCodes.Status417ExpectationFailed);
             }
-
         }
 
 
-        [HttpPatch()]                                                       //Update
+        [HttpPut()]                                                       //Update
         public IActionResult Update([FromBody] Model.Company value)
         {
             int Id = value.Id;
@@ -158,76 +158,5 @@ namespace RestApiModel.Controllers
                 return StatusCode(StatusCodes.Status405MethodNotAllowed);
             }
         }
-
-        /*
-                        [HttpPost()]                                                            //Create
-                        public IActionResult Add([FromBody] Model.Company value)
-                        {
-                            string name = value.Name;
-                            bool bsuccess = false;
-                            try
-                            {
-                                bsuccess = repo.CreateCompany(name);
-                                if (bsuccess)
-                                {
-                                    return StatusCode(StatusCodes.Status201Created);
-                                }
-                                else
-                                {
-                                    return StatusCode(StatusCodes.Status204NoContent);
-                                }
-                            }
-                            catch (Helper.RepoException ex)
-                            {
-                                switch (ex.Type)
-                                {
-                                    case EnumResultTypes.INVALIDARGUMENT:
-                                        return StatusCode(StatusCodes.Status405MethodNotAllowed);
-                                    case EnumResultTypes.NOTFOUND:
-                                        return StatusCode(StatusCodes.Status204NoContent);
-                                    case EnumResultTypes.SQLERROR:
-                                        return StatusCode(StatusCodes.Status408RequestTimeout);
-                                    case EnumResultTypes.ERROR:
-                                        return StatusCode(StatusCodes.Status500InternalServerError);
-                                    default:
-                                        return StatusCode(StatusCodes.Status501NotImplemented);
-                                }
-                            }
-                        }
-
-
-                        [HttpPatch()]                                                       //Update
-                        public IActionResult Update([FromBody] Model.Company value)
-                        {
-                            bool bsuccess = repo.UpdateCompany(value);
-                            if (bsuccess)
-                            {
-                                return StatusCode(StatusCodes.Status200OK);
-                            }
-                            else
-                            {
-                                return StatusCode(StatusCodes.Status400BadRequest);
-                            }
-                        }
-                        [HttpDelete()]                                                       //Delete
-                        public IActionResult Delete([FromBody] Model.Company value)
-                        {
-
-
-                            bool bsuccess = repo.DeleteCompany(value);
-                            if (bsuccess)
-                            {
-                                return StatusCode(StatusCodes.Status200OK);
-                            }
-                            else
-                            {
-                                return StatusCode(StatusCodes.Status400BadRequest);
-                            }
-                        }
-
-
-
-
-                    */
     }
 }
