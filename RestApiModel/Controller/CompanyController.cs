3eaf5247c7e340e6edd91401da5e07319af0953b
realfakeuser <tim.niewerth@tobit.software>
2018-09-17 11:17:17 +0200

    renamed function

--- RestApiModel/Controller/CompanyController.cs
+++ RestApiModel/Controller/CompanyController.cs
@@ -24,7 +24,7 @@ namespace RestApiModel.Controllers
 
         //CompanyRepo repo = new CompanyRepo();
         [HttpGet()]                                                             //Read
-        public IActionResult Read()
+        public IActionResult GetCompany()
         {
             var names = _companyRepo.Read();
             if (names.Count > 0)
